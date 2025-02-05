47c4a9391703f5cda5e3ddc17c2db33d25ecdd0b
Boshu Lei <leiboshu@seas.upenn.edu>
2025-02-04 18:47:55 -0800

    update all yaml.

---
+++ scripts/hiera_real_disp_stress_cnorm.sh
@@ -0,0 +1,3 @@
+python3 train_ViT_lightning.py --config configs/hiera_3.yaml \
+--ckpt_path 'exp/DT_Ultra/checkpoints/last.ckpt' \
+ --finetune --exp_name exp/hiera_disp_stress_cnorm_scale_100_100_100_loss_1_10_10 \
